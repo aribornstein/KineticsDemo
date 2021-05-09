2ebbf4854cb4fb2eeedf0606334dd14abc5c19b1
Ari Bornstein <aribornstein@gmail.com>
2021-05-09 14:32:59 +0300

    kinetics demo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# KineticsDemo
+# Flash Kinetics Demo
+Flash Video Classification Demo Coco with option for training on custom data.
+
+``` 
+python train.py --max_epochs 5 --gpus 1 --train_folder {train images} --train_ann_file {json annotations file}
+```
---
+++ train.py
@@ -0,0 +1,114 @@
+
+import os
+import sys
+from typing import Callable, List
+
+import torch
+from torch.utils.data.sampler import RandomSampler
+
+import flash
+from flash.core.classification import Labels
+from flash.core.finetuning import NoFreeze
+from flash.data.utils import download_data
+from flash.utils.imports import _KORNIA_AVAILABLE, _PYTORCHVIDEO_AVAILABLE
+from flash.video import VideoClassificationData, VideoClassifier
+
+if _PYTORCHVIDEO_AVAILABLE and _KORNIA_AVAILABLE:
+    import kornia.augmentation as K
+    from pytorchvideo.transforms import ApplyTransformToKey, RandomShortSideScale, UniformTemporalSubsample
+    from torchvision.transforms import CenterCrop, Compose, RandomCrop, RandomHorizontalFlip
+else:
+    print("Please, run `pip install torchvideo kornia`")
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--seed', type=int, default=1234)
+    parser.add_argument('--backbone', type=str, default="resnet50")
+    parser.add_argument('--download', type=bool, default=True)
+    parser.add_argument('--train_folder', type=str, default=os.path.join(os.getcwd(),
+                        "data/kinetics/train"))
+    parser.add_argument('--val_folder', type=str, default=os.path.join(os.getcwd(),
+                        "data/kinetics/val"))
+    parser.add_argument('--predict_folder', type=str, default=os.path.join(os.getcwd(),
+                        "data/kinetics/predict"))
+
+    parser.add_argument('--max_epochs', type=int, default=1)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
+    parser.add_argument('--gpus', type=int, default=None)
+    args = parser.parse_args()
+
+
+    # 1. Download the data
+    if args.download:
+        # Dataset Credit:Download a video clip dataset. 
+        # Find more datasets at https://pytorchvideo.readthedocs.io/en/latest/data.html
+        download_data("https://pl-flash-data.s3.amazonaws.com/kinetics.zip", 
+                      os.path.join(os.getcwd(), "data/")))
+
+
+    # 2. Specify transforms to be used during training.
+    # Flash helps you to place your transform exactly where you want.
+    # Learn more at https://lightning-flash.readthedocs.io/en/latest/general/data.html#flash.data.process.Preprocess
+    post_tensor_transform = [UniformTemporalSubsample(8), RandomShortSideScale(min_size=256, max_size=320)]
+    per_batch_transform_on_device = [K.Normalize(torch.tensor([0.45, 0.45, 0.45]), torch.tensor([0.225, 0.225, 0.225]))]
+
+    train_post_tensor_transform = post_tensor_transform + [RandomCrop(244), RandomHorizontalFlip(p=0.5)]
+    val_post_tensor_transform = post_tensor_transform + [CenterCrop(244)]
+    train_per_batch_transform_on_device = per_batch_transform_on_device
+
+    def make_transform(
+        post_tensor_transform: List[Callable] = post_tensor_transform,
+        per_batch_transform_on_device: List[Callable] = per_batch_transform_on_device
+    ):
+        return {
+            "post_tensor_transform": Compose([
+                ApplyTransformToKey(
+                    key="video",
+                    transform=Compose(post_tensor_transform),
+                ),
+            ]),
+            "per_batch_transform_on_device": Compose([
+                ApplyTransformToKey(
+                    key="video",
+                    transform=K.VideoSequential(
+                        *per_batch_transform_on_device, data_format="BCTHW", same_on_frame=False
+                    )
+                ),
+            ]),
+        }
+
+
+    # 3. Load the data from directories.
+    datamodule = VideoClassificationData.from_folders(
+        train_folder=args.train_folder,
+        val_folder=args.val_folder,
+        predict_folder=args.predict_folder,
+        train_transform=make_transform(train_post_tensor_transform),
+        val_transform=make_transform(val_post_tensor_transform),
+        predict_transform=make_transform(val_post_tensor_transform),
+        batch_size=8,
+        clip_sampler="uniform",
+        clip_duration=2,
+        video_sampler=RandomSampler,
+        decode_audio=False,
+    )
+
+    # 4. List the available models
+    print(VideoClassifier.available_models())
+    # out: ['efficient_x3d_s', 'efficient_x3d_xs', ... ,slowfast_r50', 'x3d_m', 'x3d_s', 'x3d_xs']
+    print(VideoClassifier.get_model_details("x3d_xs"))
+
+    # 5. Build the model - `x3d_xs` comes with `nn.Softmax` by default for their `head_activation`.
+    model = VideoClassifier(model="x3d_xs", num_classes=datamodule.num_classes)
+    model.serializer = Labels()
+
+    # 6. Finetune the model
+    trainer = flash.Trainer(max_epochs=args.max_epochs, gpus=args.gpus)
+    trainer.finetune(model, datamodule=datamodule, strategy=NoFreeze())
+    trainer.save_checkpoint("video_classification.pt")
+    
+    # 7. Make a prediction
+    predictions = model.predict(args.predict_folder)
+    print(predictions)
