1bf6430c756634264af5fa3638d8c006679d346c
tchaton <thomas@grid.ai>
2021-05-12 11:37:35 +0100

    update

--- grid.sh
+++ grid.sh
@@ -1,9 +1,8 @@
 grid train \
-    --g_name hasty-cobra-769 \
+    --g_name $1 \
     --g_disk_size 200 \
     --g_max_nodes 10 \
     --g_instance_type t2.medium \
-    --g_use_spot \
     --g_gpus 0 \
     train.py \
     --fast_dev_run 1
